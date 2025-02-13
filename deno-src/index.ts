005c69da57dc1224ca9dcbb9e21a617ab8df876b
Huakun Shen <huakun.shen@huakunshen.com>
2025-02-13 09:28:47 -0500

    refactor: update Deno RPC interface and package files configuration

--- deno-src/index.ts
+++ deno-src/index.ts
@@ -1,8 +1,9 @@
 import { expose } from "@kunkun/api/runtime/deno";
 import { wakeOnLan } from "@hk/wol/deno";
+import type { API } from "../src/types.ts";
 
 expose({
-  wakeOnLan: (mac: string, ip: string, port: number) => {
-    wakeOnLan(mac, ip, port);
+  wakeOnLan: (mac: string, ip: string, port?: number) => {
+    return wakeOnLan(mac, ip, port);
   },
-});
+} satisfies API);
